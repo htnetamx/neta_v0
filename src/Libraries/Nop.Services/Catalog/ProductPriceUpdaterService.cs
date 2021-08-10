42c08e16cddbce05e4f568df002ff6cca40971a3
APS96 <32958004+APS96@users.noreply.github.com>
2021-08-10 11:32:21 -0500

    Price Updater Service

---
+++ src/Libraries/Nop.Services/Catalog/ProductPriceUpdaterService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Services.Tasks;
+
+namespace Nop.Services.Catalog
+{
+    public partial class ProductPriceUpdaterService : IScheduleTask
+    {
+
+        #region Fields
+
+        private readonly IProductService _productService;
+
+        #endregion
+
+        #region Ctor
+
+        public ProductPriceUpdaterService(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        #endregion
+
+        public async System.Threading.Tasks.Task ExecuteAsync()
+        {
+            if (DateTime.UtcNow.Hour != 5)
+                return;
+
+            var products = (await _productService.GetAllProductsDisplayedOnHomepageAsync())
+                .Where(p => p.MarkAsNew && p.AvailableEndDateTimeUtc.Value.Date <= DateTime.UtcNow.Date);
+            foreach (var product in products)
+            {
+                if (product.OldPrice != product.Price)
+                {
+                    product.Price = product.OldPrice;
+                    await _productService.UpdateProductAsync(product);
+                }
+            }
+        }
+    }
+}
