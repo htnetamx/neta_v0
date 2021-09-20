ba263450745e5a32799fbf2afe9b6bd2ac77fe55
APS96 <32958004+APS96@users.noreply.github.com>
2021-09-20 17:33:39 -0500

    Changes

--- src/Presentation/Nop.Web/Components/HomepageProducts.cs
+++ src/Presentation/Nop.Web/Components/HomepageProducts.cs
@@ -109,8 +109,8 @@ namespace Nop.Web.Components
 
             var defaultIndex = "1";
             var defaultOrderBy = "0";
-            var defaultPageSize = "10";
-            var defaultIncrement = "10";
+            var defaultPageSize = "200";
+            var defaultIncrement = "100";
             var defaultMinimumProducts = "2";
 
             var pageSize = HttpUtility.ParseQueryString(myUri.Query).Get("pagesize");
