26114f84f53cf33a6ce58b82ce483cbd8d63d4c3
APS96 <32958004+APS96@users.noreply.github.com>
2021-09-22 11:47:30 -0500

    Front-Fixes

---
+++ src/Presentation/Nop.Web/Models/Common/HeaderFinalModel.cs
@@ -0,0 +1,26 @@
+﻿using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Stores;
+using Nop.Web.Framework.Models;
+
+namespace Nop.Web.Models.Common
+{
+    public partial record HeaderFinalModel : BaseNopModel
+    {
+        public bool IsAuthenticated { get; set; }
+        public string CustomerName { get; set; }
+        
+        public bool ShoppingCartEnabled { get; set; }
+        public int ShoppingCartItems { get; set; }
+    
+        public decimal ShoppingCartTotal { get; set; }
+        public Store Store { get; set; }
+
+        public bool WishlistEnabled { get; set; }
+        public int WishlistItems { get; set; }
+
+        public bool AllowPrivateMessages { get; set; }
+        public string UnreadPrivateMessages { get; set; }
+        public string AlertMessage { get; set; }
+        public UserRegistrationType RegistrationType { get; set; }
+    }
+}
