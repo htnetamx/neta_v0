305eeee923b1e551d3b587048dc7d826b87a8798
htnetamx <hugo@neta.mx>
2021-10-09 23:19:24 -0300

    Change: Neta discount as Neta Profit Fix: If Store has display order equal 0, except to the route

--- src/Libraries/Nop.Services/Common/PdfService.cs
+++ src/Libraries/Nop.Services/Common/PdfService.cs
@@ -737,7 +737,7 @@ namespace Nop.Services.Common
             var orderComissionInCustomerCurrency = _currencyService.ConvertCurrency(comission, 1);
             var orderComissionStr = await _priceFormatter.FormatPriceAsync(orderComissionInCustomerCurrency, true, "PES", false, languageId);
 
-            var pComission = GetPdfCell($"Descuento Neta (10%): {orderComissionStr}", titleFont);
+            var pComission = GetPdfCell($"Tus ganancias con Neta: {orderComissionStr}", titleFont);
             pComission.HorizontalAlignment = Element.ALIGN_RIGHT;
             pComission.Border = Rectangle.NO_BORDER;
             totalsTable.AddCell(pComission);
