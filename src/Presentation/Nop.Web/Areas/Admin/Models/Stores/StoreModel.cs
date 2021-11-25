f62f7e3bc972adcfc34dd9cdd7a5f7c11596f828
htnetamx <hugo@neta.mx>
2021-11-25 19:01:55 -0300

    Fix: Change NopResourceDisplayName to DisplayName

--- src/Presentation/Nop.Web/Areas/Admin/Models/Stores/StoreModel.cs
+++ src/Presentation/Nop.Web/Areas/Admin/Models/Stores/StoreModel.cs
@@ -96,10 +96,10 @@ namespace Nop.Web.Areas.Admin.Models.Stores
         [DisplayName("Domingo")]
         public DateTime? SundayEndDateTimeUtc { get; set; }
 
-        [NopResourceDisplayName("Habilita Comisión 2.0")]
+        [DisplayName("Habilita Comisión 2.0")]
         public virtual bool Comm20 { get; set; }
 
-        [NopResourceDisplayName("Monto Comisión 2.0")]
+        [DisplayName("Monto Comisión 2.0")]
         public decimal AmountComm20 { get; set; }
 
         public IList<StoreLocalizedModel> Locales { get; set; }
